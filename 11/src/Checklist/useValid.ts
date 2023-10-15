05c2d71394f74fe4c9e748bb0f9177d771424d19
phanthithanhtu <phanthithanhtu0211@gmail.com>
2023-10-15 21:59:28 +0700

    ok

--- 11/src/Checklist/useValid.ts
+++
@@ -1,7 +0,0 @@
-function validate<T>(value: T): boolean {
-    return value !== undefined && value !== null && value !== '';
-  }
-  export function useValid(data: Record<string | number, boolean>): boolean {
-    const areAnyChecked = Object.values(data).some((isChecked) => isChecked);
-    return areAnyChecked;
-  }
